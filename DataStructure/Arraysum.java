9a1abadacce97a763e26aa95fc13e275584ab26b
MOHAMMED WASIM KHAN <108329802+VENUWASIM@users.noreply.github.com>
2025-04-30 23:30:11 +0530

    Update Arraysum.java

--- DataStructure/Arraysum.java
+++ DataStructure/Arraysum.java
@@ -4,6 +4,7 @@ public class Arraysum {
 
 //Time Complexity: O(n)
 //Auxiliary Space: O(1)
+      // arry sum
     int num[]={7,6,5,4,3,2,1};
     int sum=Arrays.stream(num).sum();
     System.out.println(sum);
