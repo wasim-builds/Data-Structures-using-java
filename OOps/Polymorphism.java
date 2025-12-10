7e93ca094ba66d050fbc38a9a6a22477895dad8c
MOHAMMED WASIM KHAN <108329802+wasim-builds@users.noreply.github.com>
2025-12-10 22:53:20 +0530

    Update Polymorphism.java

--- OOps/Polymorphism.java
+++ OOps/Polymorphism.java
@@ -25,19 +25,7 @@ public class Polymorphism {
         Vehicle v2 = new Bike();
         
         v1.drive();  // Output: Driving a car...
-        v2.drive();  // Output: Riding a bike...
-    }
-}
+        v2.drive();  // Output: Riding 
 
-
-// 3. Polymorphism (One Interface, Many Forms)
-// 🔹 Real-Life Example: Vehicles (Car, Bike, Truck)
-// Imagine a driver using different types of vehicles:
-
-// A car runs on 4 wheels
-
-// A bike runs on 2 wheels
-
-// A truck runs on 6+ wheels
 
 // Regardless of the type, the driver uses the same "Drive" action, but the behavior changes based on the vehicle.
