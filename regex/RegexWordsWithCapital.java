b5d295b7e061096ba68b8b9c6ed75c7fe2141603
MOHAMMED WASIM KHAN <108329802+wasim-builds@users.noreply.github.com>
2026-02-17 19:53:45 +0530

    Update RegexWordsWithCapital.java

--- regex/RegexWordsWithCapital.java
+++ regex/RegexWordsWithCapital.java
@@ -4,6 +4,7 @@ public class RegexWordsWithCapital {
         String text = "Hello World, this Is Java.";
         String pattern = "\\b[A-Z][a-z]*\\b";
         Matcher matcher = Pattern.compile(pattern).matcher(text);
+
         while (matcher.find()) {
             System.out.println("Word starting with capital: " + matcher.group());
         }
